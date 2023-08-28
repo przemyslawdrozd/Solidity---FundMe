163a68003b8d1623d666dc3297a8324069ee28d2
Przemo <przemo06133@gmail.com>
2023-08-28 20:43:18 +0200

    Init commit

---
+++ FundMe.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FundMe {
+
+    // uint256 public myValue = 1;
+
+    function fund() public payable {
+
+        // myValue += 2;
+        require(msg.value > 1e18, "Didn't send enough ETH");
+    }
+
+    // function withdraw() public {}
+}
