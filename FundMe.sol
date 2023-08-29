9c8b299502999f2ed18d5484aa419318a04bf59a
Przemo <przemo06133@gmail.com>
2023-08-29 22:46:38 +0200

    Add Aggregator with ETH / USD latest price address

--- FundMe.sol
+++ FundMe.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+ 
 pragma solidity ^0.8.18;
 
 contract FundMe {
 
-    uint256 public myValue = 1;
+    uint256 public minimumUsd = 5;
 
     function fund() public payable {
 
-        myValue += 2;
-        require(msg.value > 1e18, "Didn't send enough ETH");
+
+
+        require(msg.value > minimumUsd, "Didn't send enough ETH");
     }
 
     // function withdraw() public {}
+
+
+    function getVersion() public view returns(uint256) {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
