585b6e8e1b44a38b2bc29ccaab71ab61ce641b8d
Przemo <przemo06133@gmail.com>
2023-08-30 17:41:29 +0200

    Required at least 5 usd in eth to fund

--- FundMe.sol
+++ FundMe.sol
@@ -6,20 +6,27 @@ pragma solidity ^0.8.18;
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
 
     function fund() public payable {
-
-
-
-        require(msg.value > minimumUsd, "Didn't send enough ETH");
+        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough ETH");
     }
 
     // function withdraw() public {}
 
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 
     function getVersion() public view returns(uint256) {
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }
