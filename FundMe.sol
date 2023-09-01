128725b39f3654c8a8c27156f33a9857bddceb97
Przemo <przemo06133@gmail.com>
2023-09-01 21:17:50 +0200

    Add payable and stict to owner

--- FundMe.sol
+++ FundMe.sol
@@ -11,6 +11,12 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough ETH");
         funders.push(msg.sender);
@@ -18,11 +24,23 @@ contract FundMe {
     }
 
     function withdraw() public {
+        require(owner == msg.sender, "Not owner!");
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        
+
+        // transfer will throw when failed
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send will return false when failed
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
+
     }
 }
